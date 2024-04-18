7cde1dab28e656cd00604578e5c66be2afb85c81
chinnadhurai <120986639+chinnadhuraisankar@users.noreply.github.com>
2024-04-18 15:01:52 -0700

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 git lfs install
 TEMP_REPO="temp"
+rm -rf ${TEMP_REPO}
 MODELS_FOLDER="models"
-git clone https://huggingface.co/slicexai/elm-v0.1 ${TEMP_REPO}
+rm -rf ${MODELS_FOLDER}
 mkdir -p $MODELS_FOLDER
+
+#repo download
+git clone https://huggingface.co/slicexai/elm-v0.1_news_classification ${TEMP_REPO}
 mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
 rm -rf ${TEMP_REPO}
+
+#repo download
+git clone https://huggingface.co/slicexai/elm-v0.1_news_summarization ${TEMP_REPO}
+mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+rm -rf ${TEMP_REPO}
+
+#repo download
+git clone https://huggingface.co/slicexai/elm-v0.1_toxicity_detection ${TEMP_REPO}
+mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+rm -rf ${TEMP_REPO}
+
+#repo download
+git clone https://huggingface.co/slicexai/elm-v0.1_news_content_generation ${TEMP_REPO}
+mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+rm -rf ${TEMP_REPO}
+
 echo "Downloaded models!"
