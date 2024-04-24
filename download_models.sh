fc76bf0d1b5e98264a40319bc9ee872aba62f2ee
chinnadhurai sankar <chinnadhurai@slicex.ai>
2024-04-24 22:10:52 +0000

    initial commit

--- download_models.sh
+++ download_models.sh
@@ -8,22 +8,22 @@ mkdir -p $MODELS_FOLDER
 
 #repo download
 git clone https://huggingface.co/slicexai/elm-v0.1_news_classification ${TEMP_REPO}
-mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+mv ${TEMP_REPO}/elm-* $MODELS_FOLDER/
 rm -rf ${TEMP_REPO}
 
 #repo download
 git clone https://huggingface.co/slicexai/elm-v0.1_news_summarization ${TEMP_REPO}
-mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+mv ${TEMP_REPO}/elm-* $MODELS_FOLDER/
 rm -rf ${TEMP_REPO}
 
 #repo download
 git clone https://huggingface.co/slicexai/elm-v0.1_toxicity_detection ${TEMP_REPO}
-mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+mv ${TEMP_REPO}/elm-* $MODELS_FOLDER/
 rm -rf ${TEMP_REPO}
 
 #repo download
 git clone https://huggingface.co/slicexai/elm-v0.1_news_content_generation ${TEMP_REPO}
-mv ${TEMP_REPO}/models/* $MODELS_FOLDER/
+mv ${TEMP_REPO}/elm-* $MODELS_FOLDER/
 rm -rf ${TEMP_REPO}
 
 echo "Downloaded models!"
